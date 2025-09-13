a19aa337a2033d70833743103287df324af67868
sandip2060 <cdysandip31@gmail.com>
2025-09-13 18:11:26 +0545

    cc

---
+++ server/controllers/RoomController.js
@@ -0,0 +1,34 @@
+import Hotel from "../models/Hotel";
+
+
+// API to create a new room for a hotel
+export const createRoom = async (req, res)=>{
+    try {
+        const {roomType, pricePerNight, amenities} = req.body;
+        const hotel = await Hotel.findOne({owner: req.auth.userId})
+
+        if(!hotel) return res.json({ success: false, message: "No Hotel found"});
+
+        
+    } catch (error) {
+        
+    }
+}
+
+
+// API to get all rooms
+export const getRooms = async (req, res)=>{
+    
+}
+
+
+// API to get a specific hotel
+export const getOwnerRooms = async (req, res)=>{
+    
+}
+
+
+// API to to toggle availability of a room
+export const toggleRoomAvailability = async (req, res)=>{
+    
+}
--- server/server.js
+++ server/server.js
@@ -4,9 +4,13 @@ import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routes/userRoutes.js";
+import hotelRouter from "./routes/hotelRoutes.js";
+import connectCloudinary from "./configs/cloudinary.js";
 
 
 connectDB()
+connectCloudinary();
 
 
 const app = express()
@@ -18,9 +22,12 @@ app.use(express.json())
 app.use(clerkMiddleware())
 
 // API to listen for clerk webhooks
-app.use("/api/clerk", clerkWebhooks)
+app.use("/api/clerk", clerkWebhooks);
 
-app.get("/", (req, res) => res.send("API is working"))
+app.get('/', (req, res) => res.send("API is working"))
+app.use("/api/user", userRouter)
+app.use("/api/hotels", hotelRouter)
+
 
 const PORT = process.env.PORT || 3000;
 
